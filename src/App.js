ae7922540b4ef842a04ae2668e5fc895db5e3953
Kaeneth Dayao <kendayao@gmail.com>
2020-11-14 12:53:01 -0800

    styled row componenet and added the rest of movie categories

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
     <div className="App">
       <Row title="NETFLIX ORIGINALS" fetch={API.fetchNetflixOriginals} />
       <Row title="Trending Now" fetch={API.fetchTrending} />
+      <Row title="Top Rated" fetch={API.fetchTopRated} />
+      <Row title="Action Movies" fetch={API.fetchActionMovies} />
+      <Row title="Comedy Movies" fetch={API.fetchComedyMovies} />
+      <Row title="Horror Movies" fetch={API.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetch={API.fetchRomanceMovies} />
+      <Row title="Documentaries" fetch={API.fetchDocumentaries} />
     </div>
   );
 }
