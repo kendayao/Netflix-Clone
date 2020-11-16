d705647485e887bbc33a427b4895710c3c10902b
Kaeneth Dayao <kendayao@gmail.com>
2020-11-16 11:24:14 -0800

    completed banner component; added fade in feature to navbar when scrolling down

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.css';
 
 function Nav(){
+
+    const[show, handleShow]=useState(false)
+
+    useEffect(()=>{
+        window.addEventListener("scroll", ()=>{
+            if(window.scrollY>100){
+                handleShow(true)
+            }else handleShow(false)
+        });
+        return()=>{
+            window.removeEventListener("scroll")
+        }
+    }, [])
+
     return(
-        <div className="nav">
+        <div className={`nav ${show&&"nav_black"}`}>
             <img className="nav_logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo"/>
-            <img classNAme="nav_avatar" src="https://i.pinimg.com/564x/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.jpg" alt="Profile Logo"/>
+            <img className="nav_avatar" src="https://i.pinimg.com/564x/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.jpg" alt="Profile Logo"/>
         </div>
     )
 }
